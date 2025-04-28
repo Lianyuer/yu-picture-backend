1856f5a484b6e158d2031075e6e8d889b60bb7c5
lianyu <lian_cx@163.com>
2025-04-29 02:02:18 +0800

    init: 项目初始化

---
+++ src/main/java/com/yu/yupicturebackend/common/BaseResponse.java
@@ -0,0 +1,34 @@
+package com.yu.yupicturebackend.common;
+
+import com.yu.yupicturebackend.exception.ErrorCode;
+import lombok.Data;
+
+/**
+ * 全局响应封装类
+ *
+ * @param <T>
+ */
+@Data
+public class BaseResponse<T> {
+
+    private int code;
+
+    private T data;
+
+    private String message;
+
+    public BaseResponse(int code, T data, String message) {
+        this.code = code;
+        this.data = data;
+        this.message = message;
+    }
+
+    public BaseResponse(int code, T data) {
+        this(code, data, "");
+    }
+
+    public BaseResponse(ErrorCode errorCode) {
+        this(errorCode.getCode(), null, errorCode.getMessage());
+    }
+
+}
---
+++ src/main/java/com/yu/yupicturebackend/common/PageRequest.java
@@ -0,0 +1,33 @@
+package com.yu.yupicturebackend.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 通用分页请求类
+ */
+@Data
+public class PageRequest implements Serializable {
+
+    /**
+     * 当前页号
+     */
+    private int current = 1;
+
+    /**
+     * 页面大小
+     */
+    private int size = 10;
+
+    /**
+     * 排序字段
+     */
+    private String sortField;
+
+    /**
+     * 排序顺序（默认升序）
+     */
+    private String sortOrder = "descend";
+
+}
