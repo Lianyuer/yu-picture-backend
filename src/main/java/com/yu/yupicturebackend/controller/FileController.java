a060683009ce88b64c350dbab4b7a11905016487
lianyu <lian_cx@163.com>
2025-05-08 01:20:01 +0800

    feat: COS 上传和下载图片接口测试

---
+++ src/main/java/com/yu/yupicturebackend/controller/FileController.java
@@ -0,0 +1,94 @@
+package com.yu.yupicturebackend.controller;
+
+import com.qcloud.cos.model.COSObject;
+import com.qcloud.cos.model.COSObjectInputStream;
+import com.qcloud.cos.utils.IOUtils;
+import com.yu.yupicturebackend.annotation.AuthCheck;
+import com.yu.yupicturebackend.common.BaseResponse;
+import com.yu.yupicturebackend.common.ResultUtils;
+import com.yu.yupicturebackend.constant.UserConstant;
+import com.yu.yupicturebackend.exception.BusinessException;
+import com.yu.yupicturebackend.exception.ErrorCode;
+import com.yu.yupicturebackend.manage.CosManage;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/file")
+@Slf4j
+public class FileController {
+
+    @Resource
+    private CosManage cosManage;
+
+    /**
+     * 测试文件上传
+     *
+     * @param multipartFile
+     * @return
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @PostMapping("/test/upload")
+    public BaseResponse<String> testUploadFile(@RequestPart("file") MultipartFile multipartFile) {
+        String filename = multipartFile.getOriginalFilename();
+        String filePath = String.format("/test/%s", filename);
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(filePath, null);
+            multipartFile.transferTo(file);
+            cosManage.putObject(filePath, file);
+            // 返回可访问地址
+            return ResultUtils.success(filePath);
+        } catch (Exception e) {
+            log.error("file upload error,filePath" + filePath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            if (file != null) {
+                // 删除临时文件
+                boolean delete = file.delete();
+                if (!delete) {
+                    log.error("file delete error,filePath={}", filePath);
+                }
+            }
+        }
+    }
+
+    /**
+     * 测试文件下载
+     *
+     * @param filepath 文件路径
+     * @param response 响应对象
+     */
+    @AuthCheck(mustRole = UserConstant.ADMIN_ROLE)
+    @GetMapping("/test/download/")
+    public void testDownloadFile(String filepath, HttpServletResponse response) throws IOException {
+        COSObjectInputStream cosObjectInput = null;
+        try {
+            COSObject cosObject = cosManage.getObject(filepath);
+            cosObjectInput = cosObject.getObjectContent();
+            // 处理下载到的流
+            byte[] bytes = IOUtils.toByteArray(cosObjectInput);
+            // 设置响应头
+            response.setContentType("application/octet-stream;charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + filepath);
+            // 写入响应
+            response.getOutputStream().write(bytes);
+            response.getOutputStream().flush();
+        } catch (Exception e) {
+            log.error("file download error, filepath = " + filepath, e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "下载失败");
+        } finally {
+            if (cosObjectInput != null) {
+                cosObjectInput.close();
+            }
+        }
+    }
+
+}
