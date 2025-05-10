fcddbb2dccdeb0591298e20d48a2c459e55791b2
lianyu <lian_cx@163.com>
2025-05-10 10:12:44 +0800

    feat: 通用图片上传下载方法封装

--- src/main/java/com/yu/yupicturebackend/manager/CosManager.java
+++ src/main/java/com/yu/yupicturebackend/manager/CosManager.java
@@ -1,10 +1,11 @@
-package com.yu.yupicturebackend.manage;
+package com.yu.yupicturebackend.manager;
 
 import com.qcloud.cos.COSClient;
 import com.qcloud.cos.model.COSObject;
 import com.qcloud.cos.model.GetObjectRequest;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.ciModel.persistence.PicOperations;
 import com.yu.yupicturebackend.config.CosClientConfig;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +13,7 @@ import javax.annotation.Resource;
 import java.io.File;
 
 @Component
-public class CosManage {
+public class CosManager {
 
     @Resource
     private CosClientConfig cosClientConfig;
@@ -22,11 +23,12 @@ public class CosManage {
 
     /**
      * 上传对象
-     * @param key 唯一键
+     *
+     * @param key  唯一键
      * @param file 文件
      * @return
      */
-    public PutObjectResult putObject(String key, File file){
+    public PutObjectResult putObject(String key, File file) {
         PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key, file);
         return cosClient.putObject(putObjectRequest);
     }
@@ -41,5 +43,22 @@ public class CosManage {
         return cosClient.getObject(getObjectRequest);
     }
 
+    /**
+     * 上传对象（附带图像信息）
+     *
+     * @param key  唯一键
+     * @param file 文件
+     * @return
+     */
+    public PutObjectResult putPictureObject(String key, File file) {
+        PutObjectRequest putObjectRequest = new PutObjectRequest(cosClientConfig.getBucket(), key, file);
+        // 对图片进行处理（获取基本信息也被视作一种图片的处理）
+        PicOperations picOperations = new PicOperations();
+        // 1 表示返回原图信息
+        picOperations.setIsPicInfo(1);
+        // 构造处理参数
+        putObjectRequest.setPicOperations(picOperations);
+        return cosClient.putObject(putObjectRequest);
+    }
 
 }
---
+++ src/main/java/com/yu/yupicturebackend/manager/FileManager.java
@@ -0,0 +1,116 @@
+package com.yu.yupicturebackend.manager;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.NumberUtil;
+import cn.hutool.core.util.RandomUtil;
+import com.qcloud.cos.model.PutObjectResult;
+import com.qcloud.cos.model.ciModel.persistence.ImageInfo;
+import com.yu.yupicturebackend.config.CosClientConfig;
+import com.yu.yupicturebackend.exception.BusinessException;
+import com.yu.yupicturebackend.exception.ErrorCode;
+import com.yu.yupicturebackend.exception.ThrowUtils;
+import com.yu.yupicturebackend.model.dto.file.UploadPictureResult;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.File;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+@Service
+@Slf4j
+public class FileManager {
+
+    @Resource
+    private CosClientConfig cosClientConfig;
+
+    @Resource
+    private CosManager cosManager;
+
+    /**
+     * 上传文件
+     *
+     * @param multipartFile    文件
+     * @param uploadPathPrefix 上传路径前缀
+     * @return
+     */
+    public UploadPictureResult uploadPicture(MultipartFile multipartFile, String uploadPathPrefix) {
+        // 校验图片
+        validatePicture(multipartFile);
+        // 图片上传地址
+        String uuid = RandomUtil.randomString(16);
+        String originalFilename = multipartFile.getOriginalFilename();
+        // 文件格式：日期_uuid.文件后缀
+        String uploadFileName = String.format(("%s_%s.%s"), DateUtil.formatDate(new Date()),
+                uuid, FileUtil.getSuffix(originalFilename));
+        // 自己拼接文件上传路径，而不是使用原始文件名，可以增强安全性
+        String uploadPath = String.format("/%s/%s", uploadPathPrefix, uploadFileName);
+        // 上传文件
+        File file = null;
+        try {
+            // 上传文件
+            file = File.createTempFile(uploadPath, null);
+            multipartFile.transferTo(file);
+            PutObjectResult putObjectResult = cosManager.putPictureObject(uploadPath, file);
+            // 获取图片信息对象
+            ImageInfo imageInfo = putObjectResult.getCiUploadResult().getOriginalInfo().getImageInfo();
+            // 计算宽高
+            int picWidth = imageInfo.getWidth();
+            int picHeight = imageInfo.getHeight();
+            double picScale = NumberUtil.round((picWidth * 1.0 / picHeight), 2).doubleValue();
+            // 封装返回结果
+            UploadPictureResult uploadPictureResult = new UploadPictureResult();
+            uploadPictureResult.setUrl(cosClientConfig.getHost() + "/" + uploadPath);
+            uploadPictureResult.setName(FileUtil.mainName(multipartFile.getOriginalFilename()));
+            uploadPictureResult.setPicSize(FileUtil.size(file));
+            uploadPictureResult.setPicWidth(picWidth);
+            uploadPictureResult.setPicHeight(picHeight);
+            uploadPictureResult.setPicScale(picScale);
+            uploadPictureResult.setPicFormat(imageInfo.getFormat());
+            return uploadPictureResult;
+        } catch (Exception e) {
+            log.error("图片上传到对象存储失败", e);
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR, "上传失败");
+        } finally {
+            // 清理临时文件
+            deleteTempFile(file);
+        }
+    }
+
+    /**
+     * 校验图片
+     *
+     * @param multipartFile 文件
+     */
+    public void validatePicture(MultipartFile multipartFile) {
+        ThrowUtils.throwIf(multipartFile == null, ErrorCode.PARAMS_ERROR, "文件不能为空");
+        // 1. 校验文件大小
+        final long ONE_M = 1024 * 1024;
+        ThrowUtils.throwIf(multipartFile.getSize() > 2 * ONE_M, ErrorCode.PARAMS_ERROR, "文件大小不能超过 2MB");
+        // 2. 校验文件后缀
+        String fileSuffix = FileUtil.getSuffix(multipartFile.getOriginalFilename());
+        // 允许上传的文件列表（或集合）
+        final List<String> ALLOW_FORMAT_LIST = Arrays.asList("jpg", "jpeg", "png", "gif", "webp");
+        ThrowUtils.throwIf(!ALLOW_FORMAT_LIST.contains(fileSuffix), ErrorCode.PARAMS_ERROR, "文件类型错误");
+    }
+
+    /**
+     * 清理临时文件
+     *
+     * @param file 文件
+     */
+    public void deleteTempFile(File file) {
+        if (file == null) {
+            return;
+        }
+        boolean deleteResult = file.delete();
+        if (!deleteResult) {
+            log.error("临时文件删除失败，文件路径: {}", file.getAbsolutePath());
+        }
+    }
+
+}
