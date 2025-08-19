f1d59a8405086bd61ebee7975573b1440aaf9b55
lianyu <lian_cx@163.com>
2025-08-20 00:18:19 +0800

    feat: 自定义注解-SaSpaceCheckPermission

--- src/main/java/com/yu/yupicturebackend/manager/auth/annotation/SaSpaceCheckPermission.java
+++ src/main/java/com/yu/yupicturebackend/manager/auth/annotation/SaSpaceCheckPermission.java
@@ -1,7 +1,10 @@
 package com.yu.yupicturebackend.manager.auth.annotation;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import cn.dev33.satoken.annotation.SaMode;
 import com.yu.yupicturebackend.manager.auth.StpKit;
+import org.springframework.core.annotation.AliasFor;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -17,4 +20,37 @@ import java.lang.annotation.Target;
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface SaSpaceCheckPermission {
 
+    /**
+     * 需要校验的权限码
+     *
+     * @return 需要校验的权限码
+     */
+    @AliasFor(annotation = SaCheckPermission.class)
+    String[] value() default {};
+
+    /**
+     * 验证模式：AND | OR，默认AND
+     *
+     * @return 验证模式
+     */
+    @AliasFor(annotation = SaCheckPermission.class)
+    SaMode mode() default SaMode.AND;
+
+    /**
+     * 在权限校验不通过时的次要选择，两者只要其一校验成功即可通过校验
+     *
+     * <p>
+     * 例1：@SaCheckPermission(value="user-add", orRole="admin")，
+     * 代表本次请求只要具有 user-add权限 或 admin角色 其一即可通过校验。
+     * </p>
+     *
+     * <p>
+     * 例2： orRole = {"admin", "manager", "staff"}，具有三个角色其一即可。 <br>
+     * 例3： orRole = {"admin, manager, staff"}，必须三个角色同时具备。
+     * </p>
+     *
+     * @return /
+     */
+    @AliasFor(annotation = SaCheckPermission.class)
+    String[] orRole() default {};
 }
