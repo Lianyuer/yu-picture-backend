47f720631a97adcaa87f9e4f282fbecdf23bb5f5
lianyu <lian_cx@163.com>
2025-08-20 01:22:42 +0800

    feat: 新增全局异常处理

--- src/main/java/com/yu/yupicturebackend/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yu/yupicturebackend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.yu.yupicturebackend.exception;
 
+import cn.dev33.satoken.exception.NotLoginException;
+import cn.dev33.satoken.exception.NotPermissionException;
 import com.yu.yupicturebackend.common.BaseResponse;
 import com.yu.yupicturebackend.common.ResultUtils;
 import lombok.extern.slf4j.Slf4j;
@@ -23,6 +25,18 @@ public class GlobalExceptionHandler {
     public BaseResponse<?> businessExceptionHandler(RuntimeException e) {
         log.error("RuntimeException:", e);
         return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+
+    @ExceptionHandler(NotLoginException.class)
+    public BaseResponse<?> notLoginException(NotLoginException e) {
+        log.error("NotLoginException", e);
+        return ResultUtils.error(ErrorCode.NOT_LOGIN_ERROR, e.getMessage());
+    }
+
+    @ExceptionHandler(NotPermissionException.class)
+    public BaseResponse<?> notPermissionExceptionHandler(NotPermissionException e) {
+        log.error("NotPermissionException", e);
+        return ResultUtils.error(ErrorCode.NO_AUTH_ERROR, e.getMessage());
     }
 
 }
