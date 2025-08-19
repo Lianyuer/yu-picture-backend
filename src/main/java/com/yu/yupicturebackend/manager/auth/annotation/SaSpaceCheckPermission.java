5708f0c1bfe52282d4fb478ccbc6ac0bb1ef7825
lianyu <lian_cx@163.com>
2025-08-20 00:29:48 +0800

    fix: 修复使用自定义注解报错

--- src/main/java/com/yu/yupicturebackend/manager/auth/annotation/SaSpaceCheckPermission.java
+++ src/main/java/com/yu/yupicturebackend/manager/auth/annotation/SaSpaceCheckPermission.java
@@ -1,6 +1,5 @@
 package com.yu.yupicturebackend.manager.auth.annotation;
 
-import cn.dev33.satoken.annotation.SaCheckLogin;
 import cn.dev33.satoken.annotation.SaCheckPermission;
 import cn.dev33.satoken.annotation.SaMode;
 import com.yu.yupicturebackend.manager.auth.StpKit;
@@ -15,7 +14,7 @@ import java.lang.annotation.Target;
  * 空间权限校验
  * <p> 可标注在函数、类上（效果等同于标注在此类的所有方法上）
  */
-@SaCheckLogin(type = StpKit.SPACE_TYPE)
+@SaCheckPermission(type = StpKit.SPACE_TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.TYPE})
 public @interface SaSpaceCheckPermission {
